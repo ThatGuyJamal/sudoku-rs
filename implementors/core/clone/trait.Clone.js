86b4a4001bfc90216032de06042b2af8b4ad721b
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-02-12 17:19:55 -0500

    Updates

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sudoku":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"sudoku/gameboard/struct.Cell.html\" title=\"struct sudoku::gameboard::Cell\">Cell</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sudoku":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"sudoku/gameboard/struct.Cell.html\" title=\"struct sudoku::gameboard::Cell\">Cell</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"sudoku/gameboard/struct.Gameboard.html\" title=\"struct sudoku::gameboard::Gameboard\">Gameboard</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
